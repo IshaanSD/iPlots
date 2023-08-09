b2ae8663df110ec5b204b664631f7e10f325e2f4
IshaanSD <i3gupta@ucsd.edu>
2023-08-09 08:26:04 -0700

    Human Haps

---
+++ human_haplotype_asms_igh_loci/get_human_haps_dotplots.sh
@@ -0,0 +1,18 @@
+cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/HG00438_hap1_igh_human_igh/dotplot.html ./HG00438_hap1_igh_human_igh_dotplot.html
+cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/HG00438_hap2_igh_human_igh/dotplot.html ./HG00438_hap2_igh_human_igh_dotplot.html
+cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/HG01106_hap1_igh_human_igh/dotplot.html ./HG01106_hap1_igh_human_igh_dotplot.html
+cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/HG01106_hap2_igh_human_igh/dotplot.html ./HG01106_hap2_igh_human_igh_dotplot.html
+cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/HG03453_hap1_igh_human_igh/dotplot.html ./HG03453_hap1_igh_human_igh_dotplot.html
+cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/HG03453_hap2_igh_human_igh/dotplot.html ./HG03453_hap2_igh_human_igh_dotplot.html
+cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/NA19240_hap1_igh_human_igh/dotplot.html ./NA19240_hap1_igh_human_igh_dotplot.html
+cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/NA19240_hap2_igh_human_igh/dotplot.html ./NA19240_hap2_igh_human_igh_dotplot.html
+
+
+
+
+
+
+
+
+
+
