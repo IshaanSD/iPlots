94a17cbda5f58147adf1f2b2a2f28af580a59514
IshaanSD <i3gupta@ucsd.edu>
2023-09-25 22:32:32 -0700

    Updated hg38 plots

--- primate_ig_loci/get_primate_dotplots.sh
+++ primate_ig_loci/get_primate_dotplots.sh
@@ -1,20 +1,20 @@
-cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/bonobo_hap1_igh_human_igh/dotplot.html ./bonobo_hap1_igh_human_igh_dotplot.html
-cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/bonobo_hap2_igh_human_igh/dotplot.html ./bonobo_hap2_igh_human_igh_dotplot.html
-cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/bornean_orangutan_hap1_igh_human_igh/dotplot.html ./bornean_orangutan_hap1_igh_human_igh_dotplot.html
-cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/bornean_orangutan_hap2_igh_human_igh/dotplot.html ./bornean_orangutan_hap2_igh_human_igh_dotplot.html
-cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/frankesntein_igh_human_igh/dotplot.html ./frankesntein_igh_human_igh_dotplot.html
-cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/gorilla_hap1_igh_human_igh/dotplot.html ./gorilla_hap1_igh_human_igh_dotplot.html
-cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/gorilla_hap2_igh_human_igh/dotplot.html ./gorilla_hap2_igh_human_igh_dotplot.html
-cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/rev_bornean_orangutan_hap1_igh_human_igh/dotplot.html ./rev_bornean_orangutan_hap1_igh_human_igh_dotplot.html
-cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/sumatran_orangutan_hap1_igh_human_igh/dotplot.html ./sumatran_orangutan_hap1_igh_human_igh_dotplot.html
-cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/sumatran_orangutan_hap2_igh_human_igh/dotplot.html ./sumatran_orangutan_hap2_igh_human_igh_dotplot.html
+cp /home/i3gupta/unialigner/tandem_aligner/test_dataset/bonobo_hap1_igh_hg38_igh/dotplot.html ./bonobo_hap1_igh_hg38_igh_dotplot.html
+cp /home/i3gupta/unialigner/tandem_aligner/test_dataset/bonobo_hap2_igh_hg38_igh/dotplot.html ./bonobo_hap2_igh_hg38_igh_dotplot.html
+cp /home/i3gupta/unialigner/tandem_aligner/test_dataset/bornean_orangutan_hap1_igh_hg38_igh/dotplot.html ./bornean_orangutan_hap1_igh_hg38_igh_dotplot.html
+cp /home/i3gupta/unialigner/tandem_aligner/test_dataset/bornean_orangutan_hap2_igh_hg38_igh/dotplot.html ./bornean_orangutan_hap2_igh_hg38_igh_dotplot.html
+cp /home/i3gupta/unialigner/tandem_aligner/test_dataset/gorilla_hap1_igh_hg38_igh/dotplot.html ./gorilla_hap1_igh_hg38_igh_dotplot.html
+cp /home/i3gupta/unialigner/tandem_aligner/test_dataset/gorilla_hap2_igh_hg38_igh/dotplot.html ./gorilla_hap2_igh_hg38_igh_dotplot.html
+cp /home/i3gupta/unialigner/tandem_aligner/test_dataset/sumatran_orangutan_hap1_igh_hg38_igh/dotplot.html ./sumatran_orangutan_hap1_igh_hg38_igh_dotplot.html
+cp /home/i3gupta/unialigner/tandem_aligner/test_dataset/sumatran_orangutan_hap2_igh_hg38_igh/dotplot.html ./sumatran_orangutan_hap2_igh_hg38_igh_dotplot.html
+cp /home/i3gupta/unialigner/tandem_aligner/test_dataset/chm13_igh_hg38_igh/dotplot.html ./chm13_igh_hg38_igh_dotplot.html 
 
-
-
-
-
-
-
-
-
-
+# cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/bonobo_hap1_igh_human_igh/dotplot.html ./bonobo_hap1_igh_human_igh_dotplot.html
+# cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/bonobo_hap2_igh_human_igh/dotplot.html ./bonobo_hap2_igh_human_igh_dotplot.html
+# cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/bornean_orangutan_hap1_igh_human_igh/dotplot.html ./bornean_orangutan_hap1_igh_human_igh_dotplot.html
+# cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/bornean_orangutan_hap2_igh_human_igh/dotplot.html ./bornean_orangutan_hap2_igh_human_igh_dotplot.html
+# cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/frankesntein_igh_human_igh/dotplot.html ./frankesntein_igh_human_igh_dotplot.html
+# cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/gorilla_hap1_igh_human_igh/dotplot.html ./gorilla_hap1_igh_human_igh_dotplot.html
+# cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/gorilla_hap2_igh_human_igh/dotplot.html ./gorilla_hap2_igh_human_igh_dotplot.html
+# cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/rev_bornean_orangutan_hap1_igh_human_igh/dotplot.html ./rev_bornean_orangutan_hap1_igh_human_igh_dotplot.html
+# cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/sumatran_orangutan_hap1_igh_human_igh/dotplot.html ./sumatran_orangutan_hap1_igh_human_igh_dotplot.html
+# cp /home/i3gupta/ta2/tandem_aligner/tandem_aligner/test_dataset/sumatran_orangutan_hap2_igh_human_igh/dotplot.html ./sumatran_orangutan_hap2_igh_human_igh_dotplot.html
